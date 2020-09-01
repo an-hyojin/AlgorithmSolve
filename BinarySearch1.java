6036d0504322df8524169da88342b82bc8975189
an-hyojin <ahj1848@naver.com>
2020-09-01 23:14:20 +0900

    BinarySearch1
    개념정리하기

---
+++ BinarySearch1.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.math.BigInteger;
+
+class BinarySearch1 {
+    int[] timeArray;
+    public long solution(int n, int[] times) {
+        Arrays.sort(times);
+        timeArray = times;
+        BigInteger num = BigInteger.valueOf((long)n);
+        BigInteger time = BigInteger.valueOf(times[times.length-1]).multiply(num);
+        return binarySearch(BigInteger.ZERO, time, num);
+
+    }
+    public long binarySearch(BigInteger min, BigInteger max, BigInteger n){
+        BigInteger half = min.add(max).divide(BigInteger.valueOf(2));
+        BigInteger realMin = max;
+        while(max.compareTo(min)>=0){
+            half = min.add(max).divide(BigInteger.valueOf(2));
+            BigInteger num =BigInteger.ZERO;
+            for(int i=0 ;i < timeArray.length; i++){
+                num = num.add(half.divide(BigInteger.valueOf((long)timeArray[i])));
+            }
+
+            if(num.compareTo(n)>=0){
+
+                max = half.subtract(BigInteger.ONE);
+
+                if(realMin.compareTo(half)>0){
+                    realMin = half;
+
+                }
+            }else{
+                min = half.add(BigInteger.ONE);
+
+            }
+        }
+        return realMin.longValue();
+    }
+}
